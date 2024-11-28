49dfdc7fb35f3052848c62cab043ad64aeac7161
aurored <aurore.drouhin@le-campus-numerique.fr>
2024-11-28 16:10:10 +0100

    j'en ai marre

--- plateau.py
+++ plateau.py
@@ -1,22 +1,35 @@
-from pion import Pion
-
-
-
 
 class Plateau : #création du plateau
+    l1 = [0, 0, 0, 0, 0, 0, 0, 0]
+    l2 = [0, 0, 0, 0, 0, 0, 0, 0]
+    l3 = [0, 0, 0, 0, 0, 0, 0, 0]
+    l4 = [0, 0, 0, 0, 0, 0, 0, 0]
+    l5 = [0, 0, 0, 0, 0, 0, 0, 0]
+    l6 = [0, 0, 0, 0, 0, 0, 0, 0]
+    l7 = [0, 0, 0, 0, 0, 0, 0, 0]
+    l8 = [0, 0, 0, 0, 0, 0, 0, 0]
 
-    def __init__(self, list_pion : list, structure_globale_du_tableau : str) :
-        self.list_pion=list_pion
-        self.structure_globale_du_tableau=structure_globale_du_tableau
+    # Création de la grille
+    grille = [l1, l2, l3, l4, l5, l6, l7, l8]
 
-    def transformation_du_tableau_a_afficher(self): 
-        for pion in self.list_pion: 
-            self.structure_globale_du_tableau = self.structure_globale_du_tableau.replace(pion.position, pion.couleur + " ") 
-              
-    def affichage (self) : #affichage
-        print(self.structure_globale_du_tableau)
-    
-if __name__ == '__main__':
-    plateau1=Plateau()
-    
-    
+    def update_plateau(grille):
+        col_width = max(len(str(word)) for row in grille for word in row) + 2  # Calcul de la largeur des colonnes
+        separator = '+' + '+'.join('-' * col_width for _ in range(len(grille[0]) + 1)) + '+'
+
+        # Ajouter la ligne d'en-tête
+        header = [' '] + [chr(ord('A') + i) for i in range(len(grille[0]))]
+        print(separator)
+        print('|' + '|'.join(str(word).ljust(col_width) for word in header) + '|')
+        print(separator)
+        
+        # Imprimer les lignes avec séparateurs
+        for i, row in enumerate(grille, start=1):
+            row = [i] + row  # Ajouter les numéros de ligne
+            print('|' + '|'.join(str(word).ljust(col_width) for word in row) + '|')
+            print(separator)  # Imprimer le séparateur après chaque ligne
+
+    update_plateau(grille)
+        
+
+   def __init__(self, grille) : 
+        self.grille = grille
